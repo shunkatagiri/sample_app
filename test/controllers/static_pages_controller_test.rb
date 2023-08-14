51dceb7c65546364d656e3d8d341ca8ce5164060
shunkatagiri <shun.katagiri1214@gmail.com>
2023-08-14 18:11:31 +0900

    「Contact」ページのテストメソッドを修正
    Contactページへのリクエストが誤ってHelpページに送られていた問題を修正しました。

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,16 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get （コードを書き込む）
-    assert_response （コードを書き込む）
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
- 
+  test "should get root" do
+    get root_url 
+    assert_response :success
+  end
+
+   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -27,7 +31,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get contact" do
-    get static_pages_help_url
+    get static_pages_contact_url
     assert_response :success
     assert_select "title" , "Contact | #{@base_title}"
   end 
