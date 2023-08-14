2ed1698218f383f772908a70524c2018645d0307
shunkatagiri <shun.katagiri1214@gmail.com>
2023-08-14 17:59:25 +0900

    rootに対するテストを追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,12 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+  test "should get root" do
+    get （コードを書き込む）
+    assert_response （コードを書き込む）
   end
 
+ 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
