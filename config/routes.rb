e88ff52101608eb7424f02e9b6b92a00baf48841
shunkatagiri <shun.katagiri1214@gmail.com>
2023-08-25 15:24:08 +0900

    rootをstaticpages_homeに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root "application#hello"
-  get  "static_pages/home"
+  root  "static_pages/home"
   get  "static_pages/help"
   get  "static_pages/about"
   get  "static_pages/contact"
