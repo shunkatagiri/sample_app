640132f2529ece97040dfd9492592e36ce693e8e
shunkatagiri <shun.katagiri1214@gmail.com>
2023-09-21 17:36:25 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,17 +12,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
-    assert is_logged_in?
   end
-
+  
   test "valid signup information" do
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+        email: "user@example.com",
+        password:              "password",
+        password_confirmation: "password" } }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+      assert is_logged_in?
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.empty?
 end
