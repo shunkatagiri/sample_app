e2dc3fbbefd8b02837294cba427630df682cf577
shunkatagiri <shun.katagiri1214@gmail.com>
2023-09-21 18:26:38 +0900

    ークン生成用メソッドを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_093033) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_091514) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
