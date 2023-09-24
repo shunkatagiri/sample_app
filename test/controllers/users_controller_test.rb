5603ff5fffba0faf57cddd26823dffc958de0b25
shunkatagiri <shun.katagiri1214@gmail.com>
2023-09-24 15:41:23 +0900

    beforeフィルターを使って編集/更新ページを保護する

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
   
   test "should redirect edit when not logged in" do
@@ -22,5 +23,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get users_new_url
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
