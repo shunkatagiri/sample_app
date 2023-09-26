12a5a7aca275442a29d3b84083b74599aa63fbd9
shunkatagiri <shun.katagiri1214@gmail.com>
2023-09-26 14:03:58 +0900

    最終的なcorrect_userの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,19 +37,19 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
     end
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless @user == current_user
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
 end
