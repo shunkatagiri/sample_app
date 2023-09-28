422621cdba357d59647cd75000727b1c7badc2f2
shunkatagiri <shun.katagiri1214@gmail.com>
2023-09-28 14:40:05 +0900

    authenticated?メソッドを抽象化する

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,10 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
