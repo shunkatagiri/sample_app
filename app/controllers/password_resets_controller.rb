9f49e7df28c41685be6e37b7d68ab8dbb80e6a81
shunkatagiri <shun.katagiri1214@gmail.com>
2023-10-01 12:26:51 +0900

    パスワード再設定のupdateアクション

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -20,7 +21,24 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.error.add(:password. "can't be empty")
+      render 'edit' , status: :unprocessable_entity
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity 
+    end
+
+
   private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -32,4 +50,12 @@ class PasswordResetsController < ApplicationController
               redirect_to root_url
       end
     end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+
 end
